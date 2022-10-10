b7a537e2f9cb81fb8a13ac250664aed12dfeb2b5
AmoDinho <u14284783@tuks.co.za>
2022-10-10 22:14:41 +0200

    setuping up tabs

--- components/ui/Tabs.js
+++ components/ui/Tabs.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import * as RadixTabs from '@radix-ui/react-tabs';
+
 const Tabs = ({}) => {
-  return <></>;
+  return (
+    <RadixTabs.Root defaultValue="sign">
+      <RadixTabs.List>
+        <RadixTabs.Trigger value="signin">Signin</RadixTabs.Trigger>
+        <RadixTabs.Trigger value="signup">Signup</RadixTabs.Trigger>
+      </RadixTabs.List>
+    </RadixTabs.Root>
+  );
 };
 export default Tabs;
